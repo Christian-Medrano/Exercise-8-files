53499ece6277737be763db8712c8acc58d853f54
Christian Medrano <christianmikhael@yahoo.com>
2017-03-31 18:26:57 -0700

    Completed variable and array set

--- index.php
+++ index.php
@@ -3,9 +3,33 @@
 
 	$product = 'iPhone';
 	$models = array(
-		'',
+		'1st Gen',
 		'3G',
-		'',
+		'3GS',
+		'4',
+		'4S',
+		'5',
+		'5C, 5S',
+		'6, 6 Plus',
+		'6S, 6S Plus',
+		'SE',
+		'7, 7 Plus'
 	);
+
+	$modelYears = array(
+		'1st Gen' => 'June 29, 2007',
+		'3G' => 'July 11, 2008',
+		'3GS' => 'June 19, 2009',
+		'4' => 'June 24, 2010',
+		'4S' => 'October 14, 2011',
+		'5' => 'September 21, 2012',
+		'5C, 5S' => 'September 20, 2013',
+		'6, 6 Plus' => 'September 19, 2014',
+		'6S, 6S Plus' => 'September 25, 2015',
+		'SE' => 'March 31, 2016',
+		'7, 7 Plus' => 'September 16, 2016'
+	);
+
+	$moviesTotal = count($models);
 
  ?>
